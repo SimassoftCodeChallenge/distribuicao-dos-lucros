eb7cefd98eb286de70489c212a7328cb075d16a4
Gabriel Simas <gabrielsimas@gmail.com>
2019-10-09 09:55:05 -0300

    Criação de abstração para inserção de valores e renomeando métodos dentro do contexto de async.

--- 03-Infra/Simasoft.Challenge.Lucro.Infra/Repositorios/Bases/RepositorioBase.cs
+++ 03-Infra/Simasoft.Challenge.Lucro.Infra/Repositorios/Bases/RepositorioBase.cs
@@ -9,6 +9,7 @@ namespace Simasoft.Challenge.Lucro.Infra.Repositorios.Bases
         public abstract void Atualizar(T obj);        
         public abstract long? Inserir(T obj);
         public abstract void Inserir(IEnumerable<T> obj);
+        public abstract void Inserir(T[] obj);
         public abstract IEnumerable<T> ListarPor(object filtro);        
         public abstract IEnumerable<T> ListarTodos();
         public abstract void RepositorioConnectionFactory(string connectionString);        
--- 03-Infra/Simasoft.Challenge.Lucro.Infra/Repositorios/Bases/RepositorioBaseAsync.cs
+++ 03-Infra/Simasoft.Challenge.Lucro.Infra/Repositorios/Bases/RepositorioBaseAsync.cs
@@ -10,7 +10,8 @@ namespace Simasoft.Challenge.Lucro.Infra.Repositorios.Bases
         public abstract Task AtualizarAsync(T obj);
         public abstract Task<long> InserirAsync(T obj);
         public abstract Task InserirAsync(IEnumerable<T> obj);
-        public abstract Task<IEnumerable<T>> ListarPor(object filtro);      
+        public abstract Task InserirAsync(T[] obj);
+        public abstract Task<IEnumerable<T>> ListarPorAsync(object filtro);      
         public abstract Task<IEnumerable<T>> ListarTodosAsync(); 
         public abstract void RepositorioConnectionFactory(string connectionString);       
     }
