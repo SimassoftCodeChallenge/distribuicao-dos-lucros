b58994113212e2336f7c85b3b7b10513df4d7ec0
Gabriel Simas <gabrielsimas@gmail.com>
2019-10-10 14:34:12 -0300

    Camada de Aplicação implementada. Ainda sem testes de unidade e integracao.

--- 01-Applicacao/Simasoft.Challenge.Lucro.Aplicacao/Contratos/IAplicacaoDistribuicaoLucros.cs
+++ 01-Applicacao/Simasoft.Challenge.Lucro.Aplicacao/Contratos/IAplicacaoDistribuicaoLucros.cs
@@ -1,7 +1,10 @@
+using System.Threading.Tasks;
+using Simasoft.Challenge.Lucro.Aplicacao.Dto.DistribuicaoLucro;
+
 namespace Simasoft.Challenge.Lucro.Aplicacao.Contratos
 {
     public interface IAplicacaoDistribuicaoLucros
     {
-         
+         ResultadoDistribuicaoLucrosDto ExecutaDistribuicao(float valorDisponibilizado,float salarioMinimoNacional);
     }
 }
--- 01-Applicacao/Simasoft.Challenge.Lucro.Aplicacao/Dto/DistribuicaoLucro/ResultadoDistribuicaoLucrosDto.cs
+++ 01-Applicacao/Simasoft.Challenge.Lucro.Aplicacao/Dto/DistribuicaoLucro/ResultadoDistribuicaoLucrosDto.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Simasoft.Challenge.Lucro.Aplicacao.Dto.DistribuicaoLucro
 {
     public class ResultadoDistribuicaoLucrosDto
@@ -5,7 +7,7 @@ namespace Simasoft.Challenge.Lucro.Aplicacao.Dto.DistribuicaoLucro
         public float TotalFuncionarios { get; set; }
         public float TotalDistribuido { get; set; }
         public float TotalDisponibilizado { get; set; }
-        public float SaltoTotalFuncionarios { get; set; }
-        public ParticipacaoDto Participacoes { get; set; }
+        public float SaldoTotalDisponibilizado { get; set; }
+        public List<ParticipacaoDto> Participacoes { get; set; }
     }
 }
